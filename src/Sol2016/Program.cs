e155928716a224195af735789a7b4a66e37fedb1
Muhammed Nilifırka <41184896+muhammednlfrk@users.noreply.github.com>
2024-10-01 21:31:43 +0300

    Add LeetCode 2016's solution

---
+++ src/Sol2016/Program.cs
@@ -0,0 +1,6 @@
+﻿using Sol2016;
+
+Solution solution = new();
+Console.WriteLine(solution.MaximumDifference([7, 1, 5, 4]));
+Console.WriteLine(solution.MaximumDifference([9, 4, 3, 2]));
+Console.WriteLine(solution.MaximumDifference([1, 5, 2, 10]));
---
+++ src/Sol2016/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace Sol2016;
+
+public class Solution
+{
+    public int MaximumDifference(int[] nums)
+    {
+        int result = -1;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (i > j || nums[i] > nums[j]) continue;
+                int dif = nums[j] - nums[i];
+                if (dif > result && dif != 0) result = dif;
+            }
+        }
+
+        return result;
+    }
+}
